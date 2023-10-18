75f6e81c81e786934b1fe755307b9b621b1a4e69
Islom Turgunov <147254633+QochqorovBaxtiyor@users.noreply.github.com>
2023-10-18 17:26:31 +0500

    JS 1-dars
    27-guruh

---
+++ JS 1-dars/JS/main.js
@@ -0,0 +1,41 @@
+
+
+/*console.log("Men console oynasida ko'rinaman");*/
+/*document.write("Men body oynasida ko'rinaman");*/
+/*alert("Sayitimizga xush kelipsiz");*/
+/*console.log(5*5+5+65+55+89/9*10*1000005);*/
+
+/*let x = 5;*/
+/*let y = 8*/
+/*let z = 10*/
+/*let x = 6, y = 6, z = 2*/
+/*
+document.write(x*y-z);*/
+/*let salom = "Salom";*/
+/*let rang = "#f1f1f1"*/
+/*document.write(rang);*/
+
+/*Data types in JS */
+
+/*let  x = 5;    */
+/*let y = "JavaScript";*/
+/*document.write(typeof x);*/
+
+/*let x = 5;*/
+/*x+=5*25+65*2+9-5*3/8*/
+
+--x;
+--x;
+--x;
+--x;
+
+document.write(x)
+
+
+
+
+
+
+
+
+
